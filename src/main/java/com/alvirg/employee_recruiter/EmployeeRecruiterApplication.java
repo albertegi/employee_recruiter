25241ee34c8daac0d91265378419467dd999f936
albertegi <albertegi90@gmail.com>
2025-11-30 23:20:11 +0000

    Project Setup and Configuration ** application properties configuration file using yml. Uses postgres database and jpa ** Docker compose file setup ** env file setup

---
+++ src/main/java/com/alvirg/employee_recruiter/EmployeeRecruiterApplication.java
@@ -0,0 +1,13 @@
+package com.alvirg.employee_recruiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRecruiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRecruiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvirg/employee_recruiter/EmployeeRecruiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvirg.employee_recruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRecruiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
