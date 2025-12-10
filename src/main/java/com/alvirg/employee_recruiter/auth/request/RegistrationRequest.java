1de1cb31113c4727a8020dd61e76a080b760d13d
albertegi <albertegi90@gmail.com>
2025-12-10 21:17:32 +0000

    complement implementation of request and response

--- src/main/java/com/alvirg/employee_recruiter/auth/request/RegistrationRequest.java
+++ src/main/java/com/alvirg/employee_recruiter/auth/request/RegistrationRequest.java
@@ -1,4 +1,18 @@
 package com.alvirg.employee_recruiter.auth.request;
 
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String confirmPassword;
+
 }
