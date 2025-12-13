21d8f4b46a6c6d9a01569617027ced4e81b13569
albertegi <albertegi90@gmail.com>
2025-12-13 22:44:21 +0000

    Create CategoryRepository for data persistence and query

---
+++ src/main/java/com/alvirg/employee_recruiter/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.alvirg.employee_recruiter.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
+}
